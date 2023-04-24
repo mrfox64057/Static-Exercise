80b37bd3744570efe4512ff5a91e994cc4154978
Adam Jankowski <ghost17814@gmail.com>
2023-04-24 15:01:41 -0400

    first and final commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What fahrenheit temperature would you like to convert to celsius?");
+            var valid = double.TryParse(Console.ReadLine(),out double userInput);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("What celsius temperature would you like to convert to fahrenheit?");
+            var isValid = double.TryParse(Console.ReadLine(), out double userInput2);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var celcius = TempConverter.FahrenheitToCelsius(userInput);
+            Console.WriteLine($"Your first temperature is {celcius}");
+            var fahrenheit = TempConverter.CelciusToFahrenheit(userInput2);
+            Console.WriteLine($"Your second temperature is {fahrenheit}");
+            
+
         }
     }
 }
